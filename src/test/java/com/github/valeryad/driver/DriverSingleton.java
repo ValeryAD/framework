b9bacfbb96a6ced4aa7d0bb0ad32b04a0366dfc5
Valery Doroshko <DoroshkoValery@gmail.com>
2021-07-26 13:51:01 +0300

    add logging info about screen size

--- src/test/java/com/github/valeryad/driver/DriverSingleton.java
+++ src/test/java/com/github/valeryad/driver/DriverSingleton.java
@@ -1,6 +1,8 @@
 package com.github.valeryad.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,6 +10,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
     private static WebDriver driver;
+    private static Logger logger = LogManager.getRootLogger();
+    //TODO dell logger
+
 
     public static WebDriver getDriver() {
         if (driver == null) {
@@ -21,6 +26,8 @@ public class DriverSingleton {
                 default: {
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
+                    logger.info("height=" + driver.manage().window().getSize().height +
+                            "width="+ driver.manage().window().getSize().width);
                 }
             }
             driver.manage().window().maximize();
