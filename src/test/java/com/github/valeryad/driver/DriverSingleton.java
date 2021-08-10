13a27d649110f503a4681526fa7f3a470bcc0083
Valery Doroshko <DoroshkoValery@gmail.com>
2021-08-10 15:15:33 +0300

    improve DriverSingletone class after code rewiew

--- src/test/java/com/github/valeryad/driver/DriverSingleton.java
+++ src/test/java/com/github/valeryad/driver/DriverSingleton.java
@@ -20,24 +20,22 @@ public class DriverSingleton {
             switch (System.getProperty("browser")) {
                 case "firefox": {
                     WebDriverManager.firefoxdriver().setup();
-                    FirefoxOptions firefoxOptions = new FirefoxOptions()
+                    FirefoxOptions firefoxOptions = new FirefoxOptions()                    //for starting driver in headless mode with specified resolution
                             .addArguments("--headless", "--width=1366", "--height=1536");
-                    driver = new FirefoxDriver(firefoxOptions);
-                    logger.info(String.format("window-size=%dx%d",
-                            driver.manage().window().getSize().width,
-                            driver.manage().window().getSize().height));
+                    driver = new FirefoxDriver(firefoxOptions);                             //regular launch is: driver = new FirefoxDriver();
                 }
                 break;
                 default: {
                     WebDriverManager.chromedriver().setup();
-                    ChromeOptions chromeOptions = new ChromeOptions()
+                    ChromeOptions chromeOptions = new ChromeOptions()                        //for starting driver in headless mode with specified resolution
                             .addArguments("--headless", "window-size=1366,1536");
-                    driver = new ChromeDriver(chromeOptions);
-                    logger.info(String.format("window-size=%dx%d",
-                            driver.manage().window().getSize().width,
-                            driver.manage().window().getSize().height));
+                    driver = new ChromeDriver(chromeOptions);                                //regular launch is: driver = new FirefoxDriver();
                 }
             }
+            logger.info(String.format("window-size=%dx%d",
+                    driver.manage().window().getSize().width,
+                    driver.manage().window().getSize().height));
+            //driver.manage().window().maximize();                                           //There's not need to maximize window in headless mode
         }
         return driver;
     }
